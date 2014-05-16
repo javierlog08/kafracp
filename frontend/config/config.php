42c2be936aca879b410d0757b815e95929356c1c
Javier Giron <jgiron@panabolsa.com>
2014-05-16 11:40:50 -0500

    Follow #4 Done Theming Support for frontend

--- frontend/config/config.php
+++ frontend/config/config.php
@@ -1,23 +1,70 @@
 <?php
+/*
+|  Kafra Control Panel Configs
+|----------------------------------------------------------------------------------
+|
+| Main Configurations. (Themes, ServerName etc.)
+|
+|----------------------------------------------------------------------------------
+*/
 
 $config = [
-	'modules' => [
-        'home' => [
-            'class' => 'root\frontend\modules\home\HomeModule',
-        ],
+	'serverName'=>	"Kafra CP",
+	'themes'=>	['basic'],
+];
+
+
+
+/*
+|  Modules configs
+|----------------------------------------------------------------------------------
+|
+| If you are installing a new module add it here.
+| For convention path to your module must be:
+| 
+| 'YourModuleName' => [
+|     	'class' => 'root\frontend\modules\YourModuleName\YourModuleNameClass',
+|  ],
+| 
+|
+|----------------------------------------------------------------------------------
+*/
+$modules = [
+	'home' => [
+     	'class' => 'root\frontend\modules\home\HomeModule',
     ],
+];
+
+
+/*
+|  Core Configs
+|----------------------------------------------------------------------------------
+|
+| Don't Touch unless you know Yii2.
+|
+|----------------------------------------------------------------------------------
+*/
+
+$params = array_merge($config,$params);
+
+foreach($config['themes'] as $i=>$theme) {
+	$themePaths['@app/views'] = '@root/frontend/themes/'.$config['themes'][$i].'/main';
+	$themePaths['@root/frontend/modules'] = '@root/frontend/themes/'.$config['themes'][$i];
+
+}
+
+$core = [
+	'modules' => $modules,
     'components' => [
 	    'view' => [
 	        'theme' => [
-	            'pathMap' => [
-	                '@root/frontend/modules' => '@root/frontend/themes/basic',
-	            ],
+	            'pathMap' => $themePaths,
 	        ],
 	    ],
 	    'errorHandler' => [
 	            'errorAction' => 'home/default/error',
 	     ],
 	],
+	'params' => $params,
 ];
-
-return $config;
+return $core;
--- frontend/themes/basic/home/views/default/error.php
+++ frontend/themes/basic/home/views/default/error.php
@@ -20,7 +20,7 @@ $this->title = $name;
     </div>
 
     <p>
-       LOL Obiamente ocurrio un error.
+        The above error occurred while the Web server was processing your request.
     </p>
     <p>
         Please contact us if you think this is a server error. Thank you.
